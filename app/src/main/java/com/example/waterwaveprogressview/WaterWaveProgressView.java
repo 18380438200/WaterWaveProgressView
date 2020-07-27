d4e93994db76828cd947fc4cd6efcd1ec346d126
apple <18201497620@163.com>
2020-07-28 00:30:18 +0800

    initCommit

---
+++ app/src/main/java/com/example/waterwaveprogressview/WaterWaveProgressView.java
@@ -0,0 +1,22 @@
+package com.example.waterwaveprogressview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+/**
+ * create by libo
+ * create on 2020/7/28
+ * description 水波进度加载view
+ */
+    public class WaterWaveProgressView extends View {
+
+    public WaterWaveProgressView(Context context) {
+        super(context);
+    }
+
+    public WaterWaveProgressView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
