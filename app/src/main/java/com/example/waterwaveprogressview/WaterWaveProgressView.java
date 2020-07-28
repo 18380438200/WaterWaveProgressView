adc5db5c740fe8cafb3c79782ad615494d49aaca
apple <18201497620@163.com>
2020-07-28 13:59:30 +0800

    基本功能绘制完成

--- app/src/main/java/com/example/waterwaveprogressview/WaterWaveProgressView.java
+++ app/src/main/java/com/example/waterwaveprogressview/WaterWaveProgressView.java
@@ -5,6 +5,7 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -19,10 +20,31 @@ import androidx.annotation.Nullable;
  * description 水波进度加载view
  */
 public class WaterWaveProgressView extends View {
+    /**
+     * 圆环paint
+     */
     private Paint circlePaint;
+    /**
+     * 文本paint
+     */
     private Paint textPaint;
+    /**
+     * 波浪paint
+     */
+    private Paint wavePaint;
     private int circleWidth;
+    private int padding;
+    /**
+     * 当前量
+     */
     private int num;
+    /**
+     * 总量
+     */
+    private float totalNum = 2000;
+    private Path wavePath;
+    /** 波浪的高度 */
+    private int waveRadius = 50;
 
     public WaterWaveProgressView(Context context) {
         super(context);
@@ -39,12 +61,20 @@ public class WaterWaveProgressView extends View {
         circlePaint.setColor(getResources().getColor(R.color.deep_green));
         circlePaint.setStyle(Paint.Style.STROKE);
         circleWidth = dp2px(getContext(), 12);
+        padding = circleWidth;
         circlePaint.setStrokeWidth(circleWidth);
         circlePaint.setAntiAlias(true);
 
         textPaint = new Paint();
         textPaint.setColor(getResources().getColor(R.color.white));
         textPaint.setAntiAlias(true);
+
+        wavePaint = new Paint();
+        wavePaint.setColor(getResources().getColor(R.color.mask_green));
+        wavePaint.setStyle(Paint.Style.FILL);
+        wavePaint.setAntiAlias(true);
+
+        wavePath = new Path();
 
         runWithAnimation(872);
     }
@@ -53,10 +83,18 @@ public class WaterWaveProgressView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        RectF rectF = new RectF(circleWidth, circleWidth, getWidth()-circleWidth, getHeight()-circleWidth);
+        RectF rectF = new RectF(padding/2, padding/2, getWidth() - padding/2, getHeight() - padding/2);
         canvas.drawArc(rectF, -90, 360, false, circlePaint);
 
-        //绘制圆内文字以及设置到准确位置
+        drawWave(canvas);
+
+        drawContentText(canvas);
+    }
+
+    /**
+     * 绘制圆内文字以及设置到准确位置
+     */
+    private void drawContentText(Canvas canvas) {
         Rect rect = new Rect();
         String content;
 
@@ -64,24 +102,40 @@ public class WaterWaveProgressView extends View {
         content = num + "M";
         textPaint.getTextBounds(content, 0, content.length(), rect);
         int numHeight = rect.height();
-        canvas.drawText(content, getWidth()/2-rect.width()/2, getHeight()/2+numHeight/2, textPaint);
+        canvas.drawText(content, getWidth() / 2 - rect.width() / 2, getHeight() / 2 + numHeight / 2, textPaint);
 
         textPaint.setTextSize(60);
         content = "剩余流量";
         textPaint.getTextBounds(content, 0, content.length(), rect);
-        canvas.drawText(content, getWidth()/2-rect.width()/2, getHeight()/2-numHeight, textPaint);
+        canvas.drawText(content, getWidth() / 2 - rect.width() / 2, getHeight() / 2 - numHeight, textPaint);
 
         textPaint.setTextSize(55);
-        content = "共2GB";
+        content = "共" + totalNum / 1000 + "GB";
         textPaint.getTextBounds(content, 0, content.length(), rect);
-        canvas.drawText(content, getWidth()/2-rect.width()/2, getHeight()/2+numHeight+dp2px(getContext(), 25), textPaint);
+        canvas.drawText(content, getWidth() / 2 - rect.width() / 2, getHeight() / 2 + numHeight + dp2px(getContext(), 25), textPaint);
     }
 
-    /** *
+    /**
+     * 绘制波浪
+     */
+    private void drawWave(Canvas canvas) {
+
+        wavePath.moveTo(padding, getHeight() / 2);
+        wavePath.quadTo(getWidth() / 4, getHeight() / 2 - waveRadius, getWidth() / 2, getHeight() / 2);
+        wavePath.quadTo(getWidth() * 3 / 4, getHeight() / 2 + waveRadius, getWidth()-padding, getHeight() / 2);
+
+        RectF rectF = new RectF(padding, padding, getWidth()-padding, getHeight()-padding);
+        wavePath.addArc(rectF, 0, 180);
+
+        canvas.drawPath(wavePath, wavePaint);
+    }
+
+    /**
      * 设置文字滚动动画
+     *
      * @param number
      */
-    public void runWithAnimation(int number){
+    public void runWithAnimation(int number) {
         ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, "num", 0, number);
         objectAnimator.setDuration(1000);
         objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
