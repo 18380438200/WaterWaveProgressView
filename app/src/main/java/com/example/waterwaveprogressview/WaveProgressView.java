fd8672f32865e83e3b2003a3ee1e14d91dfbb604
apple <18201497620@163.com>
2020-07-28 23:03:04 +0800

    圆形外观完成，文本绘制完成

--- app/src/main/java/com/example/waterwaveprogressview/WaveProgressView.java
+++ app/src/main/java/com/example/waterwaveprogressview/WaveProgressView.java
@@ -1,17 +1,21 @@
 package com.example.waterwaveprogressview;
 
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import androidx.annotation.Nullable;
-
-import javax.security.auth.login.LoginException;
 
 /**
  * create by libo
@@ -23,11 +27,19 @@ public class WaveProgressView extends View {
      * 波浪paint
      */
     private Paint wavePaint;
+    /**
+     * 圆环paint
+     */
+    private Paint circlePaint;
+    /**
+     * 文本paint
+     */
+    private Paint textPaint;
     private Path wavePath;
     private int waveWidth;
     private int waveHeight;
-    /** 水面波浪数 */
-    private final int waveCount = 5;
+    /** 水面波浪数，在为偶数的情况下，波浪才会按周期平移 */
+    private final int waveCount = 4;
     /** 当前数值 */
     private int curNum;
     /** 总数值 */
@@ -39,6 +51,14 @@ public class WaveProgressView extends View {
     /** 该view尺寸大小 */
     private int rectSize;
     private final int ANIM_DURATION = 2000;
+    private float moveDistance;
+    /** 缓存bitmap */
+    private Bitmap bitmap;
+    private Canvas bitmapCanvas;
+    /**
+     * 当前量
+     */
+    private int num;
 
     public WaveProgressView(Context context) {
         super(context);
@@ -55,8 +75,19 @@ public class WaveProgressView extends View {
         wavePaint.setColor(getResources().getColor(R.color.mask_green));
         wavePaint.setStyle(Paint.Style.FILL);
         wavePaint.setAntiAlias(true);
+        wavePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+        circlePaint = new Paint();
+        circlePaint.setAntiAlias(true);
+        circlePaint.setColor(getResources().getColor(R.color.green));
+
+        textPaint = new Paint();
+        textPaint.setColor(getResources().getColor(R.color.white));
+        textPaint.setAntiAlias(true);
 
         wavePath = new Path();
+
+        runWithAnimation(872);
     }
 
     @Override
@@ -66,6 +97,7 @@ public class WaveProgressView extends View {
         waveWidth = getMeasuredWidth()/waveCount;
         waveHeight = dp2px(getContext(), 15);
         rectSize = getMeasuredWidth();
+        moveDistance = rectSize;
 
         setMeasuredDimension(rectSize, rectSize);  //宽高大小一致
     }
@@ -74,16 +106,18 @@ public class WaveProgressView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawPath(setWavePath(), wavePaint);
+        drawBitmap(canvas);
+
+        drawContentText(canvas);
     }
 
     private Path setWavePath() {
         wavePath.reset();
 
-        wavePath.moveTo(0, rectSize*(1-percent));  //起始点，y值为水流的高度
+        wavePath.moveTo(-moveDistance, rectSize*(1-percent));  //起始点，y值为水流的高度
 
         //绘制多段波浪
-        for (int i=0;i<waveCount;i++) {
+        for (int i=0;i<waveCount*2;i++) {
             wavePath.rQuadTo(waveWidth/2, waveHeight, waveWidth, 0);
             wavePath.rQuadTo(waveWidth/2, -waveHeight, waveWidth, 0);
         }
@@ -93,6 +127,18 @@ public class WaveProgressView extends View {
         wavePath.close();
 
         return wavePath;
+    }
+
+    /**
+     * 缓存bitmap，将两个canvas重叠，选取显示前景形状，背景颜色的模式
+     */
+    private void drawBitmap(Canvas canvas) {
+        bitmap = Bitmap.createBitmap(rectSize, rectSize, Bitmap.Config.ARGB_8888);
+        bitmapCanvas = new Canvas(bitmap);
+        bitmapCanvas.drawCircle(rectSize/2, rectSize/2, rectSize/2, circlePaint);
+        bitmapCanvas.drawPath(setWavePath(), wavePaint);
+
+        canvas.drawBitmap(bitmap, 0, 0, null);
     }
 
     /**
@@ -108,12 +154,12 @@ public class WaveProgressView extends View {
     }
 
     /**
-     * 进度更新动画
+     * 水波上涨动画
      */
     public void setProgressAnim(int num) {
         ValueAnimator valueAnimator = ValueAnimator.ofInt(0, num);
         valueAnimator.setDuration(ANIM_DURATION);
-        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -128,21 +174,72 @@ public class WaveProgressView extends View {
      * 波浪平移动画
      */
     private void setWaveMoveAnim() {
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(100, 0);
         valueAnimator.setDuration(ANIM_DURATION);
-        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator.setInterpolator(new LinearInterpolator());
         valueAnimator.setRepeatCount(ValueAnimator.INFINITE); //无限循环
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 movePercent = (int) animation.getAnimatedValue();
 
-                Log.i("minfo", "movePercent" + movePercent);
+                moveDistance = (float)movePercent/100*rectSize;
 
                 postInvalidate();
             }
         });
         valueAnimator.start();
+    }
+
+    /**
+     * 绘制圆内文字以及设置到准确位置
+     */
+    private void drawContentText(Canvas canvas) {
+        Rect rect = new Rect();
+        String content;
+
+        textPaint.setTextSize(160);
+        content = num + "M";
+        textPaint.getTextBounds(content, 0, content.length(), rect);
+        int numHeight = rect.height();
+        canvas.drawText(content, getWidth() / 2 - rect.width() / 2, getHeight() / 2 + numHeight / 2, textPaint);
+
+        textPaint.setTextSize(60);
+        content = "剩余流量";
+        textPaint.getTextBounds(content, 0, content.length(), rect);
+        canvas.drawText(content, getWidth() / 2 - rect.width() / 2, getHeight() / 2 - numHeight, textPaint);
+
+        textPaint.setTextSize(55);
+        content = "共" + totalNum / 1000 + "GB";
+        textPaint.getTextBounds(content, 0, content.length(), rect);
+        canvas.drawText(content, getWidth() / 2 - rect.width() / 2, getHeight() / 2 + numHeight + dp2px(getContext(), 25), textPaint);
+    }
+
+    /**
+     * 设置文字滚动动画
+     *
+     * @param number
+     */
+    public void runWithAnimation(int number) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, "num", 0, number);
+        objectAnimator.setDuration(1000);
+        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        objectAnimator.start();
+
+        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                postInvalidate();
+            }
+        });
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
     }
 
     private int dp2px(Context context, float dp) {
